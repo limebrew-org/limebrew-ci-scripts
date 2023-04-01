1e088a8a6b474549d7565f5c735c9deac66ff910
deepraj1729 <deeprajbaidyaofficial@gmail.com>
2023-04-02 02:05:52 +0530

    :zap: Modified CI Scripts

--- scripts/ci/build-push-docker.sh
+++ scripts/ci/build-push-docker.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-GCLOUD_SERVICE_IMAGE_NAME=$1
-GCLOUD_TMP_JSON_PATH="/tmp/gcloud-api-key.json"
+export GCLOUD_TMP_JSON_PATH="/tmp/gcloud-api-key.json"
 
 #? Decode the service account JSON from github secrets
 gcloud_decode_service_account (){
-    echo ${{ secrets.GCLOUD_ENCODED_API_KEY_LIMEBREW }} | base64 -d > $GCLOUD_TMP_JSON_PATH
+    echo $GCLOUD_ENCODED_SERVICE_ACCOUNT_JSON | base64 -d > $GCLOUD_TMP_JSON_PATH
 }
 
 #? Login to Docker using the service account
 gcloud_docker_login(){
-    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://${{ secrets.GCLOUD_DOCKER_REGISTRY_HOSTNAME }}
+    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://$GCLOUD_DOCKER_REPO_HOST_NAME
 }
 
 #? Set the image name and tag
 gcloud_set_image_tag() {
-    export SERVICE_IMAGE_NAME=$GCLOUD_SERVICE_IMAGE_NAME
-    export TAG_NAME=1.0.${{ github.run_number }}
-    export GCR_IMAGE_NAME=${{ secrets.GCLOUD_DOCKER_REGISTRY_HOSTNAME }}/${{ secrets.GCLOUD_PROJECT_ID_LIMEBREW }}/${{ secrets.GCLOUD_DOCKER_REPO_NAME }}/$SERVICE_IMAGE_NAME
+    export GCR_IMAGE_NAME=$GCLOUD_DOCKER_REPO_HOST_NAME/$GCLOUD_PROJECT_ID/$GCLOUD_DOCKER_REPO_NAME/$SERVICE_IMAGE_NAME
 }
 
 #? Build the docker image and set tag
--- scripts/ci/get-build-tag.sh
+++ scripts/ci/get-build-tag.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+BUILD_NUMBER=$1
+
 PROJECT_VERSION=$(mvn -q \
                   -Dexec.executable="echo" \
-                  -DBUILD_NUMBER=${{ github.run_number }} \
+                  -DBUILD_NUMBER=$BUILD_NUMBER \
                   -Dexec.args='${project.version}' \
                   --non-recursive \
                   org.codehaus.mojo:exec-maven-plugin:1.6.0:exec)
