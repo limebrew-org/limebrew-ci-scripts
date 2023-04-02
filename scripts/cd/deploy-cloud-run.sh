20e7adca1c88c7e7e7825cbe867bf56b57ece781
deepraj1729 <deeprajbaidyaofficial@gmail.com>
2023-04-02 19:02:29 +0530

    :zap: Modified Cloud run deployment script

--- scripts/cd/deploy-cloud-run.sh
+++ scripts/cd/deploy-cloud-run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-SERVICE_IMAGE_NAME=$1
-TAG_NAME=$2
-ENVS=$3
-SERVICE_ACCOUNT=$4
-
-
 #? Set the image
 GCR_IMAGE_NAME=$GCLOUD_LOCATION-docker.pkg.dev/$GCLOUD_PROJECT_ID/$GCLOUD_DOCKER_REPO_NAME/$SERVICE_IMAGE_NAME:$TAG_NAME
 GCR_IMAGE_NAME_WITH_TAG=$GCR_IMAGE_NAME:$TAG_NAME
@@ -16,7 +10,7 @@ gcloud_list_image(){
 
 #? Push image to cloud run
 gcloud_deploy_cloud_run(){
-    gcloud run deploy $SERVICE_NAME --region $GCLOUD_LOCATION --image $GCR_IMAGE_NAME_WITH_TAG --allow-unauthenticated --set-env-vars $ENVS --service-account $SERVICE_ACCOUNT
+    gcloud run deploy $SERVICE_NAME --region $GCLOUD_LOCATION --image $GCR_IMAGE_NAME_WITH_TAG --allow-unauthenticated --set-env-vars $ENVS --service-account $DEPLOYMENT_SERVICE_ACCOUNT
 }
 
 gcloud_list_image
