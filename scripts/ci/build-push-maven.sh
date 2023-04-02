a2c17b0871fa0c29494164ff004881b3d009fea8
saptak <dassaptak0@gmail.com>
2023-04-02 17:04:06 +0530

    :memo: removed maven secret from bash script

--- scripts/ci/build-push-maven.sh
+++ scripts/ci/build-push-maven.sh
@@ -2,8 +2,5 @@
 
 BUILD_NUMBER=$1
 
-#? set the Secret to push to artifact registry
-export MAVEN_ARTIFACT_CREDENTIALS=$MAVEN_SECRET
-
 #? Push artifact to artifact registry
 mvn -s settings.xml -X -B deploy -DskipTests -DBUILD_NUMBER=$BUILD_NUMBER -e
